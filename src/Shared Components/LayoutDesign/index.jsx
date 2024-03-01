0aa3c009376ad82e737fe2dad71b32264bcec1c6
Chauhan <achauha20@Tagicsus-MacBook-Air-10.local>
2024-03-01 12:12:29 +0530

    side bar

--- src/Shared Components/LayoutDesign/index.jsx
+++ src/Shared Components/LayoutDesign/index.jsx
@@ -3,39 +3,42 @@ import Box from "@mui/material/Box";
 import CssBaseline from "@mui/material/CssBaseline";
 import NavbarDrawer from "../NavbarDrawer";
 import Header from "../Header";
-import Body from "../Body";
 import Styles from "./styles.module.css";
 import { drawerWidth } from "../../globalization/globalConstants";
-import UserManagement from "../../Main/User Management";
 
-function ResponsiveDrawer() {
-  const [mobileOpen, setMobileOpen] = React.useState(false);
-  const [isClosing, setIsClosing] = React.useState(false);
-  const handleDrawerToggle = () => {
-    if (!isClosing) {
-      setMobileOpen(!mobileOpen);
-    }
-  };
+function ResponsiveDrawer({ showSidebarAndHeader, children }) {
+    const [mobileOpen, setMobileOpen] = React.useState(false);
+    const [isClosing, setIsClosing] = React.useState(false);
+    const handleDrawerToggle = () => {
+        if (!isClosing) {
+            setMobileOpen(!mobileOpen);
+        }
+    };
 
   return (
-    <Box className={Styles.mainContainer}>
-      <CssBaseline />
-      <Header handleDrawerToggle={handleDrawerToggle} />
-      <Box
-        component="nav"
-        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
-        aria-label="mailbox folders"
-      >
-        <NavbarDrawer
-          setIsClosing={setIsClosing}
-          mobileOpen={mobileOpen}
-          setMobileOpen={setMobileOpen}
-        />
+      <Box className={Styles.mainContainer}>
+          <CssBaseline />
+          {showSidebarAndHeader && (
+              <React.Fragment>
+                  <Header handleDrawerToggle={handleDrawerToggle} />
+                  <Box
+                      component="nav"
+                      sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+                      aria-label="mailbox folders"
+                  >
+                      <NavbarDrawer
+                          setIsClosing={setIsClosing}
+                          mobileOpen={mobileOpen}
+                          setMobileOpen={setMobileOpen}
+                      />
+                  </Box>
+              </React.Fragment>
+          )}
+
+          <Box component="main" className={Styles.main}>
+              {children}
+          </Box>
       </Box>
-      <Box component="main" className={Styles.main}>
-        <UserManagement />
-      </Box>
-    </Box>
   );
 }
 
