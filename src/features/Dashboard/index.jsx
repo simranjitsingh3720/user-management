8a33d8c0cb3a13c67fe61bcde728c0fc87f14619
Pushpender Singh <36353150+pushpendersingh97@users.noreply.github.com>
2024-06-11 16:49:53 +0530

    Custom table COde

--- src/features/Dashboard/index.jsx
+++ src/features/Dashboard/index.jsx
@@ -69,6 +69,29 @@ const columnsArr = [
 ]
 const rows = [
   {id: 1,  name: "John Doe", age: '23', gender: 'Male'},
+  {id: 2,  name: "John Doe", age: '23', gender: 'Male'},
+  {id: 3,  name: "John Doe", age: '23', gender: 'Male'},
+  {id: 4,  name: "John Doe", age: '23', gender: 'Male'},
+  {id: 5,  name: "John Doe", age: '23', gender: 'Male'},
+  {id: 6,  name: "John Doe", age: '23', gender: 'Male'},
+  {id: 7,  name: "John Doe", age: '23', gender: 'Male'},
+  {id: 8,  name: "John Doe", age: '23', gender: 'Male'},
+  {id: 9,  name: "John Doe", age: '23', gender: 'Male'},
+  {id: 10,  name: "John Doe", age: '23', gender: 'Male'},
+  {id: 11,  name: "John Doe", age: '23', gender: 'Male'},
+  {id: 21,  name: "John Doe", age: '23', gender: 'Male'},
+  {id: 31,  name: "John Doe", age: '23', gender: 'Male'},
+  {id: 41,  name: "John Doe", age: '23', gender: 'Male'},
+  {id: 51,  name: "John Doe", age: '23', gender: 'Male'},
+  {id: 61,  name: "John Doe", age: '23', gender: 'Male'},
+  {id: 71,  name: "John Doe", age: '23', gender: 'Male'},
+  {id: 81,  name: "John Doe", age: '23', gender: 'Male'},
+  {id: 91,  name: "John Doe", age: '23', gender: 'Male'},
+  {id: 111,  name: "John Doe", age: '23', gender: 'Male'},
+  {id: 112,  name: "John Doe", age: '23', gender: 'Male'},
+  {id: 113,  name: "John Doe", age: '23', gender: 'Male'},
+  {id: 12,  name: "John Doe", age: '23', gender: 'Male'},
+  {id: 13,  name: "John Doe", age: '23', gender: 'Male'},
 ];
 
 const Dashboard = () => {
