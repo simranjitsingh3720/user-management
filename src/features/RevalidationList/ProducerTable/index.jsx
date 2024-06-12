728ddef5a93665863a0b4329b0aabfa3c6c06ed0
Pushpender Singh <36353150+pushpendersingh97@users.noreply.github.com>
2024-06-12 15:21:31 +0530

    COde Review Comments added

--- src/features/RevalidationList/ProducerTable/index.jsx
+++ src/features/RevalidationList/ProducerTable/index.jsx
@@ -2,53 +2,21 @@ import React, { useEffect, useState } from "react";
 import useRevalidationList from "../hooks/useRevalidationList";
 import CustomTable from "../../../components/CustomTable";
 import { Checkbox, FormControlLabel, TableCell, TableRow } from "@mui/material";
+import generateTableHeaders from "../utils/generateTableHeaders";
 
 const ProducerTable = ({ revalidationList, revalidationListLoading }) => {
   const { revalidationListUpdateData } = useRevalidationList();
   const [selectAll, setSelectAll] = useState(false);
 
-  const HEADER_COLUMNS = [
-    {
-      id: "name",
-      value: "User Name",
-      sortable: true,
-    },
-    {
-      id: "emailId",
-      value: "Email ID",
-      sortable: true,
-    },
-    {
-      id: "mobileNo",
-      value: "Mobile Number",
-      sortable: true,
-    },
-    {
-      id: "action",
-      value: "Action",
-      action: [
-        {
-          component: "switch",
-          onClick: (data, row) => {
-            data.map((item) => {
-              if (item.id === row.id) {
-                return row.checked = !row.checked;
-              }
-
-              return row.checked;
-            });
-            handleDataUpdate(data);
-            const allActive = revalidationList.every((row) => row.checked);
-            setSelectAll(allActive);
-          },
-        },
-      ],
-    },
-  ];
-
   const handleDataUpdate = (updatedData) => {
     revalidationListUpdateData(updatedData);
   };
+
+  const HEADER_COLUMNS = generateTableHeaders(
+    handleDataUpdate,
+    revalidationList,
+    setSelectAll
+  );
 
   useEffect(() => {
     const allActive = revalidationList.every((row) => row.checked);
