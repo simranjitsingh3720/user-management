c40fa00c53375c21ad63dd95a0192d732f1f44f8
mitaligemini <mitali.varshney@geminisolutions.com>
2024-06-25 18:14:26 +0530

    created shared components

---
+++ src/components/CustomAutocompleteAll/index.jsx
@@ -0,0 +1,106 @@
+import React, { useState } from 'react';
+import { Controller } from 'react-hook-form';
+import Autocomplete from '@mui/material/Autocomplete';
+import Checkbox from '@mui/material/Checkbox';
+import TextField from '@mui/material/TextField';
+import styles from './styles.module.scss';
+import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
+import CheckBoxIcon from "@mui/icons-material/CheckBox";
+import { useEffect } from 'react';
+
+const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
+const checkedIcon = <CheckBoxIcon fontSize="small" />;
+
+const AutocompleteFieldAll = ({
+  control,
+  name,
+  label,
+  required = false,
+  disabled = false,
+  options = [],
+  validation = {},
+  errors = {},
+  apiDataMap,
+  classes,
+  resetClicked
+}) => {
+  const [selectedValues, setSelectedValues] = useState([]);
+
+  useEffect(() => {
+    setSelectedValues([]);
+  }, [resetClicked]);
+
+  const handleAutocompleteChangeAll = (event, newValue) => {
+    if (newValue.some(option => option.value === 'all')) {
+      if (isCheckedAll()) {
+        setSelectedValues([]);
+      } else {
+        setSelectedValues(apiDataMap[name] || []);
+      }
+    } else {
+      setSelectedValues(newValue.filter(option => option.value !== 'all'));
+    }
+  };
+
+  const isCheckedAll = () => {
+    return selectedValues.length === (apiDataMap[name]?.length);
+  };
+
+  const isSelected = (option) => {
+    return selectedValues.some(selectedOption => selectedOption.value === option.value);
+  };
+
+  return (
+    <div className={styles.fieldContainerStyle}>
+      <div className={styles.labelText}>
+        {label} {required && <span className={styles.styledRequired}>*</span>}
+      </div>
+      <Controller
+        name={name}
+        control={control}
+        rules={{ ...validation }}
+        render={({ field }) => (
+          <Autocomplete
+            {...field}
+            multiple
+            id={name}
+            options={[{ label: 'All', value: 'all' }, ...(options || [])]}
+            disableCloseOnSelect
+            getOptionLabel={(option) => option.label}
+            value={selectedValues || []}
+            onChange={(event, newValue) => {
+              handleAutocompleteChangeAll(event, newValue);
+              field.onChange(newValue);
+              if (newValue?.length !== 0 && newValue[0]?.value === "all") {
+                field.onChange(apiDataMap[name])
+              }
+            }}
+            renderOption={(props, option) => (
+              <li {...props} key={option.value}>
+                <Checkbox
+                  icon={icon}
+                  checkedIcon={checkedIcon}
+                  style={{ marginRight: 8 }}
+                  checked={option.value === 'all' ? isCheckedAll() : isSelected(option)}
+                />
+                {option.label}
+              </li>
+            )}
+            size="small"
+            className={`${styles.customizeSelect} ${classes}`}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                placeholder="Select"
+                error={Boolean(errors[name])}
+                helperText={Boolean(errors[name]) ? 'This field is required' : ''}
+              />
+            )}
+          />
+        )}
+      />
+    </div>
+  );
+}
+
+export default AutocompleteFieldAll;
---
+++ src/components/CustomDateInput/index.jsx
@@ -0,0 +1,66 @@
+import React, { useEffect } from 'react';
+import { Controller } from 'react-hook-form';
+import { LocalizationProvider, DatePicker } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import dayjs from 'dayjs';
+import styles from "./styles.module.scss";
+
+const DateField = ({
+  control,
+  watch,
+  setValue,
+  name,
+  label,
+  required = false,
+  rules = {},
+  errors = {},
+  classes
+}) => {
+  const validateDate = (value) => {
+    if (name === 'endDate') {
+      const startDate = watch('startDate');
+      if (!startDate) {
+        return "Start date must be set before setting end date";
+      }
+      const start = dayjs(startDate, 'YYYY-MM-DD');
+      const end = dayjs(value, 'YYYY-MM-DD');
+      return end.isBefore(start.add(300, 'day'))
+        ? true
+        : "End date must be within 300 days from the start date";
+    }
+  };
+
+  return (
+    <div className={styles.fieldContainerStyle}>
+      <div className={styles.startDateStyle}>
+        <div className={styles.labelText}>
+          {label} {required && <span className={styles.styledRequired}>*</span>}
+        </div>
+        <Controller
+          name={name}
+          control={control}
+          rules={{ validate: validateDate, ...rules }}
+          render={({ field }) => (
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DatePicker
+                value={field.value ? dayjs(field.value) : null}
+                minDate={dayjs()}
+                className={`${styles.dateStyle} ${classes}`}
+                slotProps={{ textField: { size: "small" } }}
+                onChange={(date) => {
+                  const formattedDate = dayjs(date).format("YYYY-MM-DD");
+                  setValue(name, formattedDate);
+                }}
+              />
+            </LocalizationProvider>
+          )}
+        />
+        <div className={styles.styledError}>
+          {errors[name] && <span>{errors[name]?.message}</span>}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DateField;
