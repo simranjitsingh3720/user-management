b1b78b5aacb1035b24879cb60c3f29a82e2993d0
Pushpender Singh <36353150+pushpendersingh97@users.noreply.github.com>
2024-07-01 22:19:21 +0530

    UMA-520: UI || Reset button is not working properly.

--- src/features/PartnerNeft/PartnerNeftForm/index.jsx
+++ src/features/PartnerNeft/PartnerNeftForm/index.jsx
@@ -38,7 +38,8 @@ const PartnerNeftForm = () => {
     control,
     formState: { errors },
     setValue,
-    reset
+    reset,
+    resetField,
   } = useForm({
     defaultValues: {
       lob: null,
@@ -62,13 +63,24 @@ const PartnerNeftForm = () => {
       setValue("lob", data.lob);
       setValue("producer", data.producer);
 
-      const index = VERIFICATION_METHOD.findIndex(item => data.verificationMethod === item.value)
+      const index = VERIFICATION_METHOD.findIndex(
+        (item) => data.verificationMethod === item.value
+      );
       setValue("verificationMethod", VERIFICATION_METHOD[index]);
       // Fetch related products if lob is set
       if (data.lob?.id) {
         dispatch(fetchAllProductData({ lobId: data.lob.id }));
         setValue("product", data.product);
       }
+    }
+  };
+
+  const handleReset = () => {
+    if (params.id) {
+      resetField("producer");
+      resetField("verificationMethod");
+    } else {
+      reset();
     }
   };
 
@@ -80,7 +92,12 @@ const PartnerNeftForm = () => {
 
   useEffect(() => {
     dispatch(fetchLobData());
-    dispatch(fetchUser({ userType: COMMON_WORDS.PRODUCER, searchKey: COMMON_WORDS.ROLE_NAME }));
+    dispatch(
+      fetchUser({
+        userType: COMMON_WORDS.PRODUCER,
+        searchKey: COMMON_WORDS.ROLE_NAME,
+      })
+    );
   }, [dispatch]);
 
   return (
@@ -132,7 +149,7 @@ const PartnerNeftForm = () => {
                   <CustomButton
                     variant="outlined"
                     startIcon={<RestartAltIcon />}
-                    onClick={() => reset()}
+                    onClick={handleReset}
                   >
                     Reset
                   </CustomButton>
