074b6c2ca1dae00d839347b4b7c32925707c85e0
Pushpender Singh <36353150+pushpendersingh97@users.noreply.github.com>
2024-07-02 16:32:45 +0530

    Fixing Build

--- src/utils/globalConstants.js
+++ src/utils/globalConstants.js
@@ -1,6 +1,6 @@
 export const drawerWidth = 240;
 
-export const BASE_URL = "https://uat-usermgmt.tataaig.com";
+export const BASE_URL = "https://dev-usermgmt.tataaig.com";
 
 export const selectRowsData = [5, 10, 15, 20];
 export const alphaNumericRegex = /^[a-zA-Z0-9\s]*$/;
