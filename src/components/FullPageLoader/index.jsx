4cf4b7a1d5d4ac608616aae96b6f5e944e71d4e4
Pushpender Singh <36353150+pushpendersingh97@users.noreply.github.com>
2024-07-03 10:00:01 +0530

    Full Page Loader Added

--- src/components/FullPageLoader/index.jsx
+++ src/components/FullPageLoader/index.jsx
@@ -1,19 +1,21 @@
 import React from "react";
+import TataAigLoader from "./../../assets/lottie/TATA-AIG-logo-animation";
+import Lottie from "react-lottie";
 
 const FullPageLoader = () => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: TataAigLoader,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
   return (
     <div className="flex justify-center items-center h-screen">
-        <div
-        className="inline-block h-20 w-20 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
-        role="status"
-        >
-        <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
-            Loading...
-        </span>
-        </div>
-
+      <Lottie options={defaultOptions} height={100} width={100} />
     </div>
   );
-}
+};
 
 export default FullPageLoader;
