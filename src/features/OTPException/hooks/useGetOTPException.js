06ea4b2877393f2beaf15dbb030558a0d82afba6
Pushpender Singh <36353150+pushpendersingh97@users.noreply.github.com>
2024-07-06 08:20:11 +0530

    FIXED OTP EXception

--- src/features/OTPException/hooks/useGetOTPException.js
+++ src/features/OTPException/hooks/useGetOTPException.js
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
 import axiosInstance from "../../../utils/axiosInstance";
+import { buildQueryString } from "../../../utils/globalizationFunction";
+import { Search } from "@mui/icons-material";
+import { COMMON_WORDS } from "../../../utils/constants";
 
 function useGetOTPException(pageChange, rowsPage, query, searched) {
   const [data, setData] = useState(null);
@@ -12,12 +15,32 @@ function useGetOTPException(pageChange, rowsPage, query, searched) {
   const fetchData = async () => {
     try {
       setLoading(true);
-      let url = `/api/otp-exception?pageNo=${pageChange - 1}&sortKey=${
-        sort.sortKey
-      }&sortOrder=${sort.sortOrder}&pageSize=${rowsPage}`;
+
+      let params = buildQueryString({
+        pageNo: pageChange - 1,
+        sortKey: sort.sortKey,
+        sortOrder: sort.sortOrder,
+        pageSize: rowsPage,
+        childFieldsToFetch: COMMON_WORDS.PRODUCER,
+        childFieldsEdge: COMMON_WORDS.HAS_PRODUCER,
+      });
+
       if (query && searched) {
-        url += `&searchKey=${searched}&searchString=${query}`;
+        params = buildQueryString({
+          pageNo: pageChange - 1,
+          sortKey: sort.sortKey,
+          sortOrder: sort.sortOrder,
+          pageSize: rowsPage,
+          ids: query,
+          isExclusive: true,
+          edge: COMMON_WORDS.HAS_PRODUCER,
+          childFieldsToFetch: COMMON_WORDS.PRODUCER,
+          childFieldsEdge: COMMON_WORDS.HAS_PRODUCER,
+        });
       }
+
+      let url = `/api/otp-exception?${params}`;
+
       const response = await axiosInstance.get(url);
       setData(response.data);
     } catch (error) {
