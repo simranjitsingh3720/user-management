95f3bb99fcb683d0535dc9cdc5ce10c811c29656
Pushpender Singh <36353150+pushpendersingh97@users.noreply.github.com>
2024-07-09 14:47:41 +0530

    Removing extra code

--- src/features/GroupModule/constants.js
+++ src/features/GroupModule/constants.js
@@ -1,15 +1,3 @@
-export const GroupSearchBy = [
-  {
-    label: "Name",
-    value: "groupName",
-  },
-  {
-    label: "Permission Name",
-    value: "permissionName",
-  },
-];
-
-
 export const SEARCH_OPTIONS = [
   {
     label: "Group",
