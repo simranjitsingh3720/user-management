5d3504cd1bff64356b5e5411a5790ad55acbc4f8
Pushpender Singh <36353150+pushpendersingh97@users.noreply.github.com>
2024-07-13 21:46:51 +0530

    Fixed size issue

--- src/components/CustomDialog/index.jsx
+++ src/components/CustomDialog/index.jsx
@@ -12,7 +12,7 @@ const CustomDialog = ({size}) => {
   const { open, title, content, actions } = useSelector((state) => state.dialog);
 
   return (
-    <Dialog open={open} onClose={() => dispatch(hideDialog())} maxWidth={size} fullWidth={true}>
+    <Dialog open={open} onClose={() => dispatch(hideDialog())} size={size} fullWidth={true}>
       {title && <DialogTitle>{title}</DialogTitle>}
       <DialogContent>{content}</DialogContent>
       <DialogActions>
