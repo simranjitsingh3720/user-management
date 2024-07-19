f3edf03ef6697231dcb6ecf790d362de9255fa98
Pushpender Singh <36353150+pushpendersingh97@users.noreply.github.com>
2024-07-19 17:33:52 +0530

    Fixed UI terms for lob

--- src/features/Lob/utils/header.js
+++ src/features/Lob/utils/header.js
@@ -8,7 +8,7 @@ export const Header = (updateLobData) => {
     { value: "Updated At", id: "updatedAt", sortable: true},
     {
         id: "action",
-        value: "Action",
+        value: "Status",
         sortable: false,
         action: [
           {
