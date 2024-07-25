439bab1cb0dbd12571250ed42051885b3a9358b6
Pushpender Singh <36353150+pushpendersingh97@users.noreply.github.com>
2024-07-25 22:44:30 +0530

    Fixed Roles

--- src/features/RoleModule/index.jsx
+++ src/features/RoleModule/index.jsx
@@ -46,18 +46,7 @@ function RoleModule() {
   );
 
   const handleGo = () => {
-    if (query !== '') {
-      fetchRoles({
-        sortKey: orderBy,
-        sortOrder: order,
-        pageNo: page,
-        pageSize,
-        searchString: query,
-        searchKey: searched,
-      });
-    } else {
-      loadData();
-    }
+    page !== 0 ? setPage(0) : loadData();
   };
 
   const handleEditClick = useCallback(
@@ -73,7 +62,10 @@ function RoleModule() {
       pageSize,
       sortKey: orderBy,
       sortOrder: order,
+      searchString: query !== '' ? query : null,
+      searchKey:query !== '' ? searched : null,
     });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [fetchRoles, page, pageSize, orderBy, order]);
 
   useEffect(() => {
