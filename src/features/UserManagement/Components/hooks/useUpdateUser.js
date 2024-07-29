81a7e676131ba49a1f48c6fc6eb3adaab60ce7e9
mitaligemini <mitali.varshney@geminisolutions.com>
2024-07-29 12:22:02 +0530

    status issue fixed

--- src/features/UserManagement/Components/hooks/useUpdateUser.js
+++ src/features/UserManagement/Components/hooks/useUpdateUser.js
@@ -5,22 +5,30 @@ import apiUrls from '../../../../utils/apiUrls';
 import { COMMON, NAVIGATE } from '../utils/constants';
 import { useNavigate } from 'react-router-dom';
 import errorHandler from '../../../../utils/errorHandler';
+import { useDispatch } from 'react-redux';
+import { hideDialog } from '../../../../stores/slices/dialogSlice';
 
 export default function useUpdateUser() {
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
-  async function updateData(id, roleId, roleName, data) {
+  const dispatch = useDispatch();
+  async function updateData(id, roleId, roleName, data, fetchData) {
     setLoading(true);
     let payload;
     payload = {
       id: id,
-      roleId,roleName,
-      fields: data
+      roleId,
+      roleName,
+      fields: data,
     };
     try {
       const response = await axiosInstance.put(`${apiUrls.getUser}`, payload);
       toast.success(response?.data?.message || COMMON.USER_UPDATED_SUCCESS);
       navigate(NAVIGATE.NAVIGATE_TO_USER_MANAGEMENT);
+      dispatch(hideDialog());
+      if (fetchData) {
+        fetchData();
+      }
     } catch (error) {
       errorHandler.handleError(error);
     } finally {
