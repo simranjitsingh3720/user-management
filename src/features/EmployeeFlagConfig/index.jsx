cd8ce2692da57b826c4527fbbb68dbd5e25d1123
Pushpender Singh <36353150+pushpendersingh97@users.noreply.github.com>
2024-07-30 10:11:01 +0530

    refactor(employee flag config): code Optimize

--- src/features/EmployeeFlagConfig/index.jsx
+++ src/features/EmployeeFlagConfig/index.jsx
@@ -1,104 +1,98 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback, useMemo } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import useGetEmployeeFlag from './hooks/useGetEmployeeFlag';
 import CustomTable from '../../components/CustomTable';
-import { generateTableHeaders } from './utils/generateTableHeaders';
-import { COMMON_WORDS } from '../../utils/constants';
 import CustomDialog from '../../components/CustomDialog';
 import SearchComponent from '../../components/SearchComponent';
+import { generateTableHeaders } from './utils/generateTableHeaders';
+import { COMMON_WORDS } from '../../utils/constants';
 import { getPlaceHolder } from '../../utils/globalizationFunction';
-import { useDispatch, useSelector } from 'react-redux';
 import { fetchUser } from '../../stores/slices/userSlice';
 import { showDialog } from '../../stores/slices/dialogSlice';
-import Content from './Dialog/Content';
-import Actions from './Dialog/Action';
 import { setExtraColumns, setTableName } from '../../stores/slices/exportSlice';
 import { BUTTON_TEXT, PAGECOUNT } from '../../utils/globalConstants';
 import usePermissions from '../../hooks/usePermission';
 import { EXPORT_DROPDOWN_COLUMNS } from './utils/constant';
+import Content from './Dialog/Content';
+import Actions from './Dialog/Action';
 
 function EmployeeFlagConfig() {
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.user);
+  const { canCreate } = usePermissions();
+
   const [page, setPage] = useState(0);
   const [pageSize, setPageSize] = useState(PAGECOUNT);
   const [order, setOrder] = useState(COMMON_WORDS.ASC);
   const [orderBy, setOrderBy] = useState(COMMON_WORDS.CREATED_AT);
   const [tableData, setTableData] = useState([]);
-  const { user } = useSelector((state) => state.user);
-  const dispatch = useDispatch();
-
-  // Check Permission
-  const { canCreate } = usePermissions();
-
-  useEffect(() => {
-    dispatch(
-      fetchUser({
-        userType: COMMON_WORDS.PRODUCER,
-        searchKey: COMMON_WORDS.ROLE_NAME,
-      })
-    );
-  }, [dispatch]);
 
   const { data, loading, fetchData } = useGetEmployeeFlag(page, pageSize, order, orderBy);
 
   useEffect(() => {
-    if (data && data?.data) {
-      const refactorData = data?.data.map((item) => ({
-        id: item?.employeeFlagConfig?.id,
-        label: item?.employeeFlagConfig?.label,
-        producerCode: item?.producer[0]?.producerCode,
-        producerName: `${item?.producer[0]?.firstName} ${item?.producer[0]?.lastName}`,
-        productDetails: item?.products,
-        createdAt: item?.employeeFlagConfig?.createdAt,
-        updatedAt: item?.employeeFlagConfig?.updatedAt,
-      }));
+    dispatch(fetchUser({ userType: COMMON_WORDS.PRODUCER, searchKey: COMMON_WORDS.ROLE_NAME, isAll: true }));
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (data?.data) {
+      const refactorData = data.data.map((item) => {
+        const { employeeFlagConfig, producer, products } = item;
+
+        return {
+          id: employeeFlagConfig?.id,
+          label: employeeFlagConfig?.label,
+          producerCode: producer[0]?.producerCode,
+          producerName: `${producer[0]?.firstName} ${producer[0]?.lastName}`,
+          productDetails: products,
+          createdAt: employeeFlagConfig?.createdAt,
+          updatedAt: employeeFlagConfig?.updatedAt,
+        };
+      });
       setTableData(refactorData);
       dispatch(setTableName(refactorData[0]?.label));
-      dispatch(setExtraColumns(EXPORT_DROPDOWN_COLUMNS))
+      dispatch(setExtraColumns(EXPORT_DROPDOWN_COLUMNS));
     }
   }, [data, dispatch]);
 
-  const fetchIdsAndConvert = (inputData) => {
-    const ids = (inputData || []).map((producer) => producer.id);
-    return ids.join();
-  };
-
-  const optionLabelUser = (option) => {
-    return option?.firstName ? `${option?.firstName?.toUpperCase()} ${option?.lastName?.toUpperCase()}` : '';
-  };
-
-  const renderOptionUserFunction = (props, option) => (
-    <li {...props} key={option?.id}>
-      {option?.firstName?.toUpperCase()} {''}
-      {option?.lastName?.toUpperCase()}
-    </li>
+  const handleClicked = useCallback(
+    (row) => {
+      dispatch(
+        showDialog({
+          title: COMMON_WORDS.PRODUCT_DETAILS,
+          content: <Content row={row} />,
+          actions: <Actions row={row} fetchData={fetchData} />,
+        })
+      );
+    },
+    [dispatch, fetchData]
   );
 
-  const handleClicked = (row) => {
-    dispatch(
-      showDialog({
-        title: COMMON_WORDS.PRODUCT_DETAILS,
-        content: <Content row={row} />,
-        actions: <Actions row={row} fetchData={fetchData} />,
-      })
-    );
-  };
+  const HEADER_COLUMNS = useMemo(() => generateTableHeaders(handleClicked), [handleClicked]);
 
-  const HEADER_COLUMNS = generateTableHeaders(handleClicked);
-
-  const onSubmit = (data) => {
-    const resultProducersId = fetchIdsAndConvert(data.autocomplete);
-    fetchData(resultProducersId);
-  };
+  const onSubmit = useCallback(
+    (data) => {
+      const resultProducersId = data.autocomplete.map((producer) => producer.id).join();
+      fetchData(resultProducersId);
+    },
+    [fetchData]
+  );
 
   return (
     <div>
       <div className="mb-4">
         <SearchComponent
           optionsData={user?.data || []}
-          optionLabel={optionLabelUser}
+          optionLabel={(option) =>
+            option?.firstName ? `${option.firstName.toUpperCase()} ${option.lastName.toUpperCase()}` : ''
+          }
           placeholder={getPlaceHolder(COMMON_WORDS.PRODUCER)}
-          renderOptionFunction={renderOptionUserFunction}
+          renderOptionFunction={(props, option) => (
+            <li {...props} key={option?.id}>
+              {option?.firstName?.toUpperCase()} {option?.lastName?.toUpperCase()}
+            </li>
+          )}
           onSubmit={onSubmit}
-          showExportButton={true}
+          showExportButton
           buttonText={BUTTON_TEXT.EMPLOYEE_FLAG_CONFIG}
           navigateRoute="/employee-flag-config/form"
           showButton
@@ -108,7 +102,7 @@ function EmployeeFlagConfig() {
       </div>
       <CustomTable
         columns={HEADER_COLUMNS}
-        rows={tableData || []}
+        rows={tableData}
         loading={loading}
         totalCount={data?.totalCount || 0}
         page={page}
