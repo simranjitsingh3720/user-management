fedc6e2c008ec7ea4e67e4fca4475d3d6981aeb9
mitaligemini <mitali.varshney@geminisolutions.com>
2024-08-07 16:41:32 +0530

    issue fixed

--- src/components/CustomTable/TableHeader/index.jsx
+++ src/components/CustomTable/TableHeader/index.jsx
@@ -23,28 +23,27 @@ const TableHeader = ({
       <TableRow>
         {columns.map((column) => (
           <TableCell
-          key={column.id}
-          sortDirection={orderBy === column.id ? order : false}
-          style={customStyles.cell}
-        >
-          {column.sortable ? (
-            <TableSortLabel
-              active={orderBy === column.id}
-              direction={orderBy === column.id ? order : 'asc'}
-              onClick={() => createSortHandler(column.id)}
-            >
-              {column.value}
-            </TableSortLabel>
-          ) : column.showIButton ? (
-            <div className="flex">
-              {column.value}
-              <ToolTipIcon title = 'File with error logs will be downloaded' />
-            </div>
-          ) : (
-            column.value
-          )}
-        </TableCell>
-        
+            key={column.id}
+            sortDirection={orderBy === column.id ? order : false}
+            style={customStyles.cell}
+          >
+            {column.sortable ? (
+              <TableSortLabel
+                active={orderBy === column.id}
+                direction={orderBy === column.id ? order : 'asc'}
+                onClick={createSortHandler(column.id)}
+              >
+                {column.value}
+              </TableSortLabel>
+            ) : column.showIButton ? (
+              <div className="flex">
+                {column.value}
+                <ToolTipIcon title = 'File with error logs will be downloaded' />
+              </div>
+            ) : (
+              column.value
+            )}
+          </TableCell>
         ))}
       </TableRow>
     </TableHead>
