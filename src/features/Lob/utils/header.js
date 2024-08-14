510fb79c3ae10cc2d80f790ff8f6bf343dcb6657
Pushpender Singh <36353150+pushpendersingh97@users.noreply.github.com>
2024-08-14 18:30:28 +0530

    Lob Level Removed

--- src/features/Lob/utils/header.js
+++ src/features/Lob/utils/header.js
@@ -1,23 +1,22 @@
 export const Header = (handleStatusUpdate) => {
   return [
-    { value: "Lob Name", id: "lob" },
-    { value: "Lob Value", id: "lobValue" },
-    { value: "Lob Level", id: "lobLevel" },
-    { value: "Lob Code", id: "lobCode" },
-    { value: "Created At", id: "createdAt", sortable: true},
-    { value: "Updated At", id: "updatedAt", sortable: true},
+    { value: 'Lob Name', id: 'lob' },
+    { value: 'Lob Value', id: 'lobValue' },
+    { value: 'Lob Code', id: 'lobCode' },
+    { value: 'Created At', id: 'createdAt', sortable: true },
+    { value: 'Updated At', id: 'updatedAt', sortable: true },
     {
-        id: "action",
-        value: "Status",
-        sortable: false,
-        action: [
-          {
-            component: "switch",
-            onClick: (data, row) => {
-              handleStatusUpdate(data, row);
-            },
+      id: 'action',
+      value: 'Status',
+      sortable: false,
+      action: [
+        {
+          component: 'switch',
+          onClick: (data, row) => {
+            handleStatusUpdate(data, row);
           },
-        ],
-      },
+        },
+      ],
+    },
   ];
 };
