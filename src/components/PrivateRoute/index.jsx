f7fb62908f0e42b55279fe6b3dd3dafa462a1281
Pushpender Singh <36353150+pushpendersingh97@users.noreply.github.com>
2024-08-23 18:40:08 +0530

    Fixed Routing issue

--- src/components/PrivateRoute/index.jsx
+++ src/components/PrivateRoute/index.jsx
@@ -1,19 +1,40 @@
-import React, { useEffect } from "react";
-import { Navigate } from "react-router-dom";
-import { TOKEN, TOKEN_EXPIRATION_ERROR } from "../../utils/globalConstants";
-import toastifyUtils from "../../utils/toastify";
+import React, { useEffect, useMemo } from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import { TOKEN, TOKEN_EXPIRATION_ERROR } from '../../utils/globalConstants';
+import toastifyUtils from '../../utils/toastify';
+import { COMMON_WORDS } from '../../utils/constants';
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
+  const location = useLocation();
+  const path = location.pathname.split('/')[1];
   const token = localStorage.getItem(TOKEN);
-   useEffect(() => {
-     if (!token) {
-      toastifyUtils.notifyError(TOKEN_EXPIRATION_ERROR)
-     }
-   });
- 
- return (
-    token ? <Component {...rest} /> : <Navigate to="/" />
- )
+
+  const parsedScopes = useMemo(() => {
+    try {
+      const storedScopes = localStorage.getItem(COMMON_WORDS.SCOPES);
+      return storedScopes ? JSON.parse(storedScopes) : null;
+    } catch (error) {
+      console.error('Error parsing scopes:', error);
+      return null;
+    }
+  }, []);
+
+  const hasPermission = useMemo(
+    () => parsedScopes?.read.some((permission) => permission.route === path),
+    [parsedScopes, path]
+  );
+
+  useEffect(() => {
+    if (!token) {
+      toastifyUtils.notifyError(TOKEN_EXPIRATION_ERROR);
+    }
+  }, [token]);
+
+  if (!token || !hasPermission) {
+    return <Navigate to="/" />;
+  }
+
+  return <Component {...rest} />;
 };
 
 export default PrivateRoute;
