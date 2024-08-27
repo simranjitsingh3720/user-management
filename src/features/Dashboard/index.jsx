f6663e46799963505083faff18a0931e6e67e765
Pushpender Singh <36353150+pushpendersingh97@users.noreply.github.com>
2024-08-27 20:37:06 +0530

    MEssage Update

--- src/features/Dashboard/index.jsx
+++ src/features/Dashboard/index.jsx
@@ -1,10 +1,6 @@
-import React from "react";
+import React from 'react';
 const Dashboard = () => {
-  return (
-    <div>
-      Dashboard
-    </div>
-  );
+  return <div className="text-center font-bold text-2xl">Welcome to User Management</div>;
 };
 
 export default Dashboard;
